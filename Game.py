fcb125464bc66027c9b6637d74c30e98f818f718
UsmanQ132 <usmanq0139@gmail.com>
2025-07-03 17:06:09 +0500

    update

--- Game.py
+++ Game.py
@@ -1,6 +1,18 @@
 import random 
 
 
+
+# create dict for word count
+def load_dict():
+    dict ={}
+    with open('words.txt') as file:
+        for arrange in file:
+            words = arrange.strip().lower()
+            dict[words] = 0
+
+    return dict     
+dict = load_dict()
+
 def load_file():
 
     with open('words.txt') as file:
@@ -10,7 +22,17 @@ def load_file():
         # return word
         random_word = random.choice(word)
         return random_word
-        # no_of_letter = len(random_word)
+
+def update_word_count(random_word):
+    global dict
+    if random_word in dict:
+        dict[random_word] += 1
+    with open('word_count.txt', 'w') as file:
+        for word, count in dict.items():
+            file.write(f"{word}: {count}\n")
+
+
+
 
 def select_word():
     get_word = load_file()
@@ -22,27 +44,34 @@ def Player_name():
     return Player_name
 
 def Menu():
-    menus = input("Type to Start Game'Play' or 'Exit' to quit: ")
-    if menus == "play" or "Play":
+    menus = input("Type to Start Game 'Play' or 'Exit' to quit: ")
+    if menus.strip().lower() == "play":
         print('Loading...')
-    elif menus == "Exit":
+        return True
+    elif menus.strip().lower() == "exit":
         print('Quit.')
         exit()
     else:
-        print('Invaild Input')
-        Menu()
+        print('Invalid Input')
+        return False
+
 
 def game_control():
 
         user = Player_name()
 
         print(f"Welcome {user},Let's Start the Game")
-        print("You Have 3 Guesses to fill the word")
+        print("You Have 7 Guesses to fill the word")
         print("Let's Go.....")
 
-        play_game = Menu()
-        play_game
-        # return user
+        menu = Menu()
+
+        while not menu:
+            menu = Menu()
+
+        word = select_word()
+        update_word_count(word)
+
 
         # get word
         word = select_word()
@@ -58,13 +87,15 @@ def game_control():
 
 
         while no_of_Guesses > 0:
-                user_guess =  input("Enter your guess: ")
+                user_guess =  input("Enter your guess: ").strip().lower()
+                if user_guess == "":
+                    print("Please fill the field")
                 # 1 character at 1 time
                 if len(user_guess) > 1:
                     print("You can only guess one letter at a time")
                     continue
                 # when the guess is correct then print correct
-                if user_guess in word:
+                if user_guess == word:
                     print("Correct Guess")
                 # Update the dash variable to correct guesses
                 for i in range(no_of_letters):
@@ -74,6 +105,8 @@ def game_control():
                 # if all guesses are = to word then you Won the game    
                 if dash == word:
                     print(f"{user} - Won - word: {word}")
+                    with open('game_results.txt','a') as result_file:
+                        result_file.write(f"{user} - Won - word: {word}\n\n")
                     break
                 # when wrong guess decrease 1 from guess
                 if user_guess not in word:
@@ -97,6 +130,9 @@ def game_control():
         print("You have", no_of_Guesses, "guesses left")
 
         print("The word was", word)
+        with open('game_results.txt','a') as result_file:
+                        result_file.write(f"{user} - Lost - word: {word}\n")
+
 
 game_control()
 
